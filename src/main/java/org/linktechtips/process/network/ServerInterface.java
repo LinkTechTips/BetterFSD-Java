07b746dc40273d249f2172efa710766afe570367
LinkTechTips <3174327625@qq.com>
2022-11-20 01:14:00 +0800

    Fix errors when other servers connect(hotfix)

--- src/main/java/org/linktechtips/process/network/ServerInterface.java
+++ src/main/java/org/linktechtips/process/network/ServerInterface.java
@@ -329,7 +329,7 @@ public class ServerInterface extends TcpInterface {
         assemble(buf, cmdNum, to, from, bi, pc, hc, data);
 
         if (direction != null) {
-            direction.uslprintf("%u\n", slCheck);
+            direction.uslprintf("%d \n", slCheck);
             return;
         }
 
